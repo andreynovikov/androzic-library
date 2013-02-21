bdf4965170a7554a73c5993cc8bb8b406b2ef2f4
Andrey Novikov <novikov@gmail.com>
2013-02-21 20:56:05 +0400

    Simplified code

--- src/com/androzic/provider/DataContract.java
+++ src/com/androzic/provider/DataContract.java
@@ -6,16 +6,11 @@ public final class DataContract
 {
 	public static final String AUTHORITY = "com.androzic.DataProvider";
 	protected static final String MAPOBJECTS_PATH = "mapobjects";
-	public static final Uri MAPOBJECTS_URI;
+	public static final Uri MAPOBJECTS_URI = Uri.parse("content://" + AUTHORITY + "/" + MAPOBJECTS_PATH);
 	
 	public static final String[] MAPOBJECT_COLUMNS = new String[] {"latitude", "longitude", "bitmap"};
 	public static final int MAPOBJECT_LATITUDE_COLUMN = 0;
 	public static final int MAPOBJECT_LONGITUDE_COLUMN = 1;
 	public static final int MAPOBJECT_BITMAP_COLUMN = 2;
 	public static final String MAPOBJECT_ID_SELECTION = "IDLIST";
-	
-	static
-	{
-		MAPOBJECTS_URI = Uri.parse("content://" + AUTHORITY + "/" + MAPOBJECTS_PATH);
-	}
 }
