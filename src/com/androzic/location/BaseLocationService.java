325580c51cab77282145669f7310082b07db014c
Andrey Novikov <novikov@gmail.com>
2013-02-23 18:00:48 +0400

    Location service constant definitions for plugins

---
+++ src/com/androzic/location/BaseLocationService.java
@@ -0,0 +1,23 @@
+package com.androzic.location;
+
+import android.app.Service;
+
+public abstract class BaseLocationService extends Service
+{
+	/**
+	 * Broadcast sent when service status changes
+	 */
+	public static final String BROADCAST_LOCATING_STATUS = "com.androzic.locatingStatusChanged";
+	/**
+	 * GPS status code
+	 */
+	public static final int GPS_OFF = 1;
+	/**
+	 * GPS status code
+	 */
+	public static final int GPS_SEARCHING = 2;
+	/**
+	 * GPS status code
+	 */
+	public static final int GPS_OK = 3;
+}
