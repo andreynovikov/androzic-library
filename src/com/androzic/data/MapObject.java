7b827aa849a74d472416d127e72474b6eea9cc15
Andrey Novikov <novikov@gmail.com>
2014-12-02 14:58:43 +0300

    Map object marker

--- src/com/androzic/data/MapObject.java
+++ src/com/androzic/data/MapObject.java
@@ -28,12 +28,16 @@ public class MapObject
 	public String name = "";
 	public String description = "";
 	public String image = "";
+	public String marker = "";
+	public int pin = 0;
 	public boolean drawImage = false;
 	public double latitude = 0;
 	public double longitude = 0;
 	public int altitude = Integer.MIN_VALUE;
 	public int proximity = 0;
 	public Bitmap bitmap;
+	public int anchorX;
+	public int anchorY;
 	public int textcolor = Integer.MIN_VALUE;
 	public int backcolor = Integer.MIN_VALUE;
 
---
+++ src/com/androzic/data/Marker.java
@@ -0,0 +1,16 @@
+package com.androzic.data;
+
+import android.graphics.Bitmap;
+
+public class Marker
+{
+	public String name;
+	public Bitmap image;
+	public int anchorX;
+	public int anchorY;
+	
+	public Marker(String name)
+	{
+		this.name = name;
+	}
+}
