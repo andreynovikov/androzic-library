04840ff3761506d1cd051863c5ccb2cd2a5947fb
Andrey Novikov <novikov@gmail.com>
2015-01-27 14:32:42 +0300

    Copy file utility

--- src/main/java/com/androzic/util/FileUtils.java
+++ src/main/java/com/androzic/util/FileUtils.java
@@ -1,5 +1,13 @@
 package com.androzic.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class FileUtils
 {
 	public static String unusable = "*+~|<>!?\\/:";
@@ -22,5 +30,24 @@ public class FileUtils
 				sb.append(name.charAt(i));
 		}
 		return sb.toString();
+	}
+
+	public static void copyFile(File from, File to) throws IOException
+	{
+		//create output directory if it doesn't exist
+		to.getParentFile().mkdirs();
+
+		InputStream in = new FileInputStream(from);
+		OutputStream out = new FileOutputStream(to);
+
+		byte[] buffer = new byte[1024];
+		int read;
+		while ((read = in.read(buffer)) != -1)
+			out.write(buffer, 0, read);
+		in.close();
+
+		// write the output file
+		out.flush();
+		out.close();
 	}
 }
